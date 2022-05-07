89cb0938601e5cf72bbc48316a14ab77d7ca2148
mischa.huang <mischa.huang@creativearktech.com>
2022-05-07 18:21:34 +0800

    chore: add example.env

--- connections/index.js
+++ connections/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-require('dotenv').config();
+dotenv.config({path: './config.env'});
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
