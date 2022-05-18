8688117dc96c93de881b4076a27cdbc574a9d7b2
mischa.huang <mischa.huang@creativearktech.com>
2022-05-18 17:37:14 +0800

    chore: add connect db env url

--- connections/index.js
+++ connections/index.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: './config.env'});
 
-const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+const DB =
+  process.env.NODE_ENV === 'dev'
+    ? 'mongodb://localhost:27017/postDB'
+    : process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB)
-  .then(() => console.log('db connect success'))
+  .then(() => console.log('db connect success2'))
   .catch(e => console.log(e));
