b11e3dabb62165b44911cf84ab45954750496765
mischa.huang <mischa.huang@creativearktech.com>
2022-12-08 17:19:18 +0800

    feat: connection ws, define ws on event

--- service/chatroom.js
+++ service/chatroom.js
@@ -8,10 +8,63 @@ const connection = () => {
   wss.on('connection', function connection(ws) {
     console.log('wss:client connection')
 
-    ws.on('message', function incoming(data) {
-      const msg = data.toString();
+    ws.on('message', async function incoming(data) {
+      const msg = JSON.parse(data.toString());
+      let payload;
+
+      if(msg.cmd === "WEB_Init") {
+        const { name, photo, user, content} = msg;
+        payload = {
+          name,
+          photo,
+          user,
+          role: 'system',
+          cmd: 'APP_Init_Response',
+          content,
+        };
+      }
+
+      if(msg.cmd === "WEB_User_Leave") {
+        const { name, photo, user, content} = msg;
+        payload = {
+          name,
+          photo,
+          user,
+          role: 'system',
+          cmd: 'APP_User_Leave_Response',
+          content,
+        };
+      }
+
+      if(msg.cmd === "WEB_Add_Message") {
+        const { name, photo, user, content} = msg;
+        payload = {
+          name,
+          photo,
+          user,
+          role: 'user',
+          cmd: 'APP_Add_Message_Response',
+          content,
+        };
+      }
+
+      if(msg.cmd === "WEB_Typing") {
+        const { name, content } = msg;
+        payload = {
+          role: 'system',
+          name,
+          content,
+          cmd: 'APP_Typing_Response'
+        };
+      }
+      
+      let clients = wss.clients
+      clients.forEach(client => {
+        client.send(JSON.stringify(payload))
+      })
+      
       console.log('msg: ', msg);
-      ws.send(msg);
+      // ws.send(data.toString());
     })
 
     ws.on('close', () => {
