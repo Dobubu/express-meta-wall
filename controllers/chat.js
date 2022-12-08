c132b5d6d8a0802d9dc3dccfd23ac6e1475157f5
mischa.huang <mischa.huang@creativearktech.com>
2022-12-08 17:21:39 +0800

    feat: add Chat Model, save message to DB

---
+++ controllers/chat.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const Chat = require('../model/chat');
+
+const handleSuccess = require('../service/handleSuccess');
+
+const chats = {
+  async fetchMessageList(req, res) {
+    /**
+     * #swagger.tags = ['Chat']
+     * #swagger.summary = 'Get message list'
+     * #swagger.security = [{
+        "apiKeyAuth": []
+      }]
+     */
+
+    const data = await Chat.find();
+
+    handleSuccess(res, data);
+  },
+  async addMessage(payload) {
+    const { name, user, photo, role, cmd, content } = payload;
+
+    await Chat.create({
+      name,
+      user,
+      photo,
+      role,
+      cmd,
+      content
+    });
+  },
+  async deleteMessages(req, res) {
+    /**
+     * #swagger.tags = ['Chat']
+     * #swagger.summary = 'Delete chats'
+     * #swagger.security = [{
+        "apiKeyAuth": []
+      }]
+     */
+    await Chat.deleteMany({});
+
+    handleSuccess(res, []);
+  },
+};
+
+module.exports = chats;
